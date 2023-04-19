b783ad5e4ba2a160f3fe2583d67fbf536eca2578
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-20 01:36:07 +0200

    test: add test for nested class (#8)

---
+++ test/cases/nested-class.java
@@ -0,0 +1,16 @@
+class Animal {
+
+   // inner class
+   class Reptile {
+      public void displayInfo() {
+        System.out.println("I am a reptile.");
+      }
+   }
+
+   // static class
+   static class Mammal {
+      public void displayInfo() {
+        System.out.println("I am a mammal.");
+      }
+   }
+}
